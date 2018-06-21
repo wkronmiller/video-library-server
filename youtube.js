ec6480fdc0a56c8fb3e0344469a38a47a85cf1cd
William Rory Kronmiller <wrk961@gmail.com>
2018-06-21 06:53:56 -0400

    Namespace youtube videos

--- youtube.js
+++ youtube.js
@@ -18,9 +18,29 @@ function listVideos() {
   }));
 }
 
+function getVideo(videoId) {
+  const params = {
+    Key: {
+      videoId: {
+        S: videoId
+      }
+    },
+    TableName: tableName,
+  };
+  return new Promise((resolve, reject) =>
+    dynamodb.getItem(params, (err, data) => 
+      (err) ? reject(err) : resolve(data)));
+}
+
+const VIDEO_FORMAT = 'mp4';
 function getVideoInfo(videoId) {
   return ytdl.getInfo(`https://www.youtube.com/watch?v=${videoId}`)
-    .then(({ title, description, thumbnail_url: thumbnailUrl, video_url: videoUrl }) =>
+    .then(info => {
+      const { formats } = info;
+      const [{ url: videoUrl }] = formats.filter(({ container }) => container === VIDEO_FORMAT).slice(0,1);
+      return Object.assign(info, { videoUrl });
+    })
+    .then(({ title, description, thumbnail_url: thumbnailUrl, videoUrl }) =>
       ({ title, description, thumbnailUrl, videoUrl }));
 }
 
@@ -31,6 +51,9 @@ function addVideo(videoId) {
       Item: {
         videoId: {
           S: videoId
+        },
+        format: {
+          S: VIDEO_FORMAT,
         },
         title: {
           S: title
@@ -66,10 +89,26 @@ module.exports.remove = (event, context, callback) => {
   });
 };
 
+module.exports.get = ({ pathParameters: { id } }, context, callback) => {
+  getVideo(id)
+    .then(({ Item }) => Item)
+    .then(item => Object.keys(item)
+      .map(key => ({ [key]: item[key][Object.keys(item[key])[0]] }))
+      .reduce((a,b) => Object.assign(a,b), {}))
+    .then(video => callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ video })
+    }))
+    .catch(err => callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({ err }),
+    }));
+};
+
 module.exports.list = (event, context, callback) => {
   listVideos()
     .then(({ Items }) => Items)
-    .then(items => items.map(({ videoId: { S } }) => S))
+    .then(items => items.map(({ videoId: { S: videoId }, thumbnailUrl: { S: thumbnailUrl } }) => ({ videoId, thumbnailUrl })))
     .then(videos => callback(null, {
       statusCode: 200,
       body: JSON.stringify({ videos })
@@ -79,3 +118,5 @@ module.exports.list = (event, context, callback) => {
       body: JSON.stringify({ err }),
     }));
 };
+
+//TODO: generic video library with URL and thumbnail info, metadata upon request
